7a6bb502606ba9ae811515f59fd0408835bc06e0
Farhan Ahmadi <Farhanahmadiro7@gmail.com>
2022-08-20 22:29:12 +0430

    update

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@
 
 // module.exports = nextConfig
 
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 module.exports = {
   images: {
-    domains: ['45.159.113.83'],
+    domains: ["45.159.113.83"],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+};
